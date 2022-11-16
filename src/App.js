7f7dbaab45d718ffd68bbdaa44a3326d25583421
giorgi <100317242+Bucci20@users.noreply.github.com>
2022-11-16 20:05:48 +0400

    დავალება 5

--- src/App.js
+++ src/App.js
@@ -1,25 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{ useState } from "react";
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const initialValues = {name:"", lastname:"", age:"", Email:"", Gender:""};
+  const [formValues, setFormValues] = useState(initialValues);
+  const [formErrors, setFormErrors] = useState({});
+  const [isSubmit, setIsSubmit] = useState();
+  const [remove, setDelete] = useState({});
+  const handleDelete = (index,e) => {
+      setDelete(remove.filter((i) => i !== index));
+  }
+  
+
+
+const handleCHange = (e) => {
+  const {name, value} = e.target;
+  setFormValues({...formValues, [name]: value});
+};
+const handleSubmit = (e) => {
+  e.preventDefault();
+  setFormErrors(validate(formValues));
+  setIsSubmit(true)
+};
+const validate = (values) => {
+  const errors = {};
+  if(values.name.length < 4){
+  errors.name = "Name is Requiers at list 4 characters";
 }
+if (values.lastname.length< 4) {
+  errors.lastname = "Last Name is Requiers at list 4 characters";
+}
+if(!values.Email){
+  errors.Email = "Email is requierd!";
+}else if(!values.Email.includes("@")){
+  errors.Email= "This Email is not valid"
+}
+if(values.age.length < 18){
+errors.age = "User has to be over 18";
+ }
+  if(!values.Gender){
+    errors.Gender = "pleas Select your gender";
+  };
+}
+
+return(
+  <div>
+<div className="container">
+
+
+<input 
+type="text"
+name="name"
+value={formValues.name}
+onChange={handleCHange}/> <button onClick={e =>handleDelete(e) }>Delete</button> <label>Name</label>
+  
+  
+
+<div>
+
+<input 
+type="text"
+name="lastname"
+value={formValues.lastname}
+onChange={handleCHange}/> <button>Delete</button> <label>Last Name</label>
+</div>
+
+<div>
+  
+
+ <input 
+type="number"
+name="age"
+value={formValues.age}
+onChange={handleCHange}/> <button>Delete</button> <label>Age</label>
+</div>
+<div>
+
+ <input 
+type="text"
+name="Email"
+value={formValues.Email}
+onChange={handleCHange}/> <button>Delete</button> <label>Email</label>
+</div>
+
+<select onChange={handleCHange}>
+  <option value={formValues.Gender}>Male</option>
+  <option value={formValues.Gender}>Female</option>
+  <option value={formValues.Gender}>Other</option>
+  
+</select>
+
+<button onClick={isSubmit}
+type="submit"
+value={handleSubmit}
+>Submit</button>
+
+</div>
+</div>
+)
+
+
+};
 
 export default App;
